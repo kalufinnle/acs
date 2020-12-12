07a72082f067aa3fbb37b547d9047d0ea448c926
Kalu Everyone <23731053+kalufinnle@users.noreply.github.com>
2020-12-12 18:23:29 -0500

    Update exec_print_files_structure.py

--- exec_print_files_structure.py
+++ exec_print_files_structure.py
@@ -96,7 +96,7 @@ class DisplayablePath(object):
     
     
 if __name__ == '__main__': 
-    lib_path = input ("""Enter the directary you want to see the files structures. Example: 'C:\\Users\\kevin\\GitHub\FloWaveNet-master' ： """)
+    lib_path = input ("""Enter the directary you want to see the files structures. Example: C:\\\Users\\\kevin\\\GitHub\FloWaveNet-master ： """)
     
     paths = DisplayablePath.make_tree(Path(lib_path))
     
